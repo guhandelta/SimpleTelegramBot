93e58b95ffde6bad87c857ddb543953b56331cfc
Guhaprasaanth Nandagopal <guhan.delta@gmail.com>
2020-04-08 21:52:27 -0400

    Learnt about Context Shortcuts

--- bot.js
+++ bot.js
@@ -3,17 +3,6 @@ const Telegraf = require('telegraf'); // a class has been imported here, so the 
 //creating an instance of a bot using the *Telegraf* as a constructor
 const bot = Telegraf(TELEGRAF_API_KEY); // api token within the () was provided by the BotFather
 
-// Registers a middleware => Takes in a middleware fn() and hadles all req from the user
-// Evertime the user interacts with the bot, it will be handled by the use()
-bot.use((ctx, next) => { //next => callback_fn() just like in ExpressJS. next() will call the next middleware, start() in this case
-    // state allows us to pass data between middlewares > state is nothing but data stored
-    ctx.state.date = 8;
-    ctx.reply("The bot was used");
-    next(ctx); //Passes the Telegram context obj to the 
-    //next() will still do the same without the ctx passed in, but passing in ctx will allow the next middleware to handle the-
-    //-data which had been changed in the current fn() where the next() was called
-});
-
 // start(ctx,next) => ctx -> context obj is the context for one Telegram update || next -(optional)-> fn(), like a callback
 // A Telegraf Context encapsulates telegram update. Context is created per request and contains many props like edit message,....etc
 // Any msg or sticker or media sent, will be sent as a req to this script || Request(Telegraf_Context(Data))
@@ -34,6 +23,14 @@ bot.settings((ctx) => {
 // bot.command("test", (ctx) => { 
 bot.command(["test", "Test", "tst"], (ctx) => {
     ctx.reply("Custom Command Works!!!!!");
+});
+
+// Context shortcuts are another way of using Telegram methods, where the *chatId* arg need not be mentioned
+bot.command("shorcut", (ctx) => {
+    // ctx.reply("text", [extra]); => with context shortcuts => reply() is the context shortcut
+    ctx.reply("Hello World");
+    // bot.telegram.sendMessage(chatId,"text", [extra]); => using Telegram methods
+    // bot.telegram.sendMessage(ctx.chat.id, "Hello World");
 });
 
 // hears() => Registers middleware for handling text messages, and it's not a Telegram command
@@ -63,6 +60,17 @@ bot.phone("+918870784275", (ctx) => {
 //handles hashtags eg. #hash
 bot.hashtag("hash", (ctx) => {
     ctx.reply("Some Hashtag");
+});
+
+// Registers a middleware => Takes in a middleware fn() and hadles all req from the user
+// Evertime the user interacts with the bot, it will be handled by the use()
+bot.use((ctx, next) => { //next => callback_fn() just like in ExpressJS. next() will call the next middleware, start() in this case
+    // state allows us to pass data between middlewares > state is nothing but data stored
+    ctx.state.date = 8;
+    ctx.reply("The bot was used");
+    next(ctx); //Passes the Telegram context obj to the 
+    //next() will still do the same without the ctx passed in, but passing in ctx will allow the next middleware to handle the-
+    //-data which had been changed in the current fn() where the next() was called
 });
 
 //cmd to launch Telegram bot to start pulling in updates
